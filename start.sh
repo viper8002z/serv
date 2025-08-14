a54c24d57d5d0f13360b856c3ce7110687cd936d
viper8002z <viper8002z@gmail.com>
2025-08-15 02:18:32 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 # Directly run the panel binary without systemd check
+
+ARCH=$(uname -m)
+case "${ARCH}" in
+  x86_64 | x64 | amd64) XUI_ARCH="amd64" ;;
+  i*86 | x86) XUI_ARCH="386" ;;
+  armv8* | armv8 | arm64 | aarch64) XUI_ARCH="arm64" ;;
+  armv7* | armv7) XUI_ARCH="armv7" ;;
+  *) XUI_ARCH="amd64" ;;
+esac
+
+cd /root/
+wget https://github.com/alireza0/x-ui/releases/latest/download/x-ui-linux-${XUI_ARCH}.tar.gz
+rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
+tar zxvf x-ui-linux-${XUI_ARCH}.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+cp x-ui/x-ui.sh /usr/bin/x-ui
+mv x-ui/ /usr/local/
+
+
+
 /usr/local/x-ui/x-ui &
 nginx -g "daemon off;"
